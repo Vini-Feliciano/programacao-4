65a855041124b65359344bd170e7010541ef0ee2
vini-feliciano <viniciusfeliciano1206@gmail.com>
2026-05-13 22:41:17 -0300

    feat: o backend conecta ao BD

--- python/crud.py
+++ python/crud.py
@@ -1,49 +1,81 @@
-filmes = []
-id_atual = 0
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+#config do BD
+Base = declarative_base()
+engine = create_engine('sqlite:///filmes.db',echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class Filme(Base):
+    __tablename__ ="filmes"
+    id= Column(Integer,primary_key=True,autoincrement=True)
+    nome = Column(String,nullable=False)
+    genero = Column(String)
+
+    def to_dict(self):
+        return{
+            'id': self.id,
+            'nome': self.nome,
+            'genero': self.genero
+        }
+
+Base.metadata.create_all(engine)
+
 
 #CRIAR
 def criar(nome,genero):
-    global id_atual
 
     if not nome or not nome.strip():
         raise ValueError('Nome do filme é obrigatório')
 
-    novo_filme = {
-        'id': id_atual,
-        'nome': nome.strip(),
-        'genero': genero
-    }
+    novo_filme = Filme(
+        nome=nome.strip(),
+        genero=genero
+    )
 
-    filmes.append(novo_filme)
-    id_atual += 1
-    return novo_filme
+    session.add(novo_filme)
+    session.commit()
+    return novo_filme.to_dict()
 
 #LER
 def ler():
-    return filmes.copy()
+    filmes = session.query(Filme).order_by(Filme.id).all()
+    return [filme.to_dict() for filme in filmes]
 
 #buscar por id
 def buscar_por_id(id):
-    return next((f for f in filmes if f['id'] == id), None)
+    filme = session.query(Filme).filter(Filme.id == id).first()
+    return filme.to_dict() if filme else None
 
 #ATUALIZAR
 def atualizar(id,novos_dados):
     filme = buscar_por_id(id)
+
     if not filme:
         raise ValueError('Filme não encontrado')
+
     if 'nome' in novos_dados:
         nome_novo = novos_dados['nome'].strip()
         if not nome_novo:
             raise ValueError('Nome do filme não pode ser vazio')
-        filme['nome'] = nome_novo
+
+        filme.nome = nome_novo
 
     if 'genero' in novos_dados:
-        filme['genero'] = novos_dados['genero']
+        filme.genero = novos_dados['genero']
 
-    return filme
+    session.commit()
+    return filme.to_dict()
 
 #DELETAR
 def deletar(id):
-    tamanho_original = len(filmes)
-    filmes[:] = [f for f in filmes if f['id'] != id]
-    return len(filmes) < tamanho_original
+    filme = buscar_por_id(id)
+
+    if filme:
+        session.delete(filme)
+        session.commit()
+        return True
+    else:
+        return False
